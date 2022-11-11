5799b63258e7713ea0a572fdc3799aa694921f89
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-11-11 03:02:58 +0100

    boostrapped routes methods templates

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,1 +1,23 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func SignUp(c *gin.Context) {
+
+}
+
+func Login(c *gin.Context) {
+
+}
+
+func ProductViewerAdmin(c *gin.Context) {
+
+}
+
+func SearchProduct(c *gin.Context) {
+	
+}
+
+func SearchProductByQuery(c *gin.Context) {
+
+}
